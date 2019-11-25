9f17e8cb0d578157d09b851b07295120c65ba5f5
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-25 11:53:26 +0100

    Replace syfotekster with local texts in TiltakListeRad

--- js/components/oppfolgingsplan/tiltak/liste/TiltakListeRad.js
+++ js/components/oppfolgingsplan/tiltak/liste/TiltakListeRad.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getLedetekst } from '@navikt/digisyfo-npm';
 import { STATUS_TILTAK } from '../../../../konstanter';
 import { tiltakPt } from '../../../../propTypes/opproptypes';
 import { toDateMedMaanedNavn } from '../../../../utils/datoUtils';
+
+const texts = {
+    status: {
+        avtalt: 'Avtalt',
+        ikkAktuelt: 'Ikke aktuelt',
+        foreslatt: 'Foreslått',
+    },
+};
 
 const TiltakListeRad = (
     {
@@ -16,15 +23,15 @@ const TiltakListeRad = (
     switch (tiltak.status) {
         case STATUS_TILTAK.AVTALT:
             klasse = 'etikett--suksess';
-            status = 'oppfolgingsdialog.tiltak.status.godkjent';
+            status = texts.status.avtalt;
             break;
         case STATUS_TILTAK.IKKE_AKTUELT:
             klasse = 'etikett--advarsel';
-            status = 'oppfolgingsdialog.tiltak.status.ikke.aktuelt';
+            status = texts.status.ikkAktuelt;
             break;
         default:
             klasse = 'etikett--fokus';
-            status = 'oppfolgingsdialog.tiltak.status.foreslaatt';
+            status = texts.status.foreslatt;
             break;
     }
 
@@ -42,7 +49,7 @@ const TiltakListeRad = (
             { tiltak.status &&
             <div className={`tiltaktabell__rad__status etikett ${klasse}`} >
                 <span className="typo-normal">
-                    {getLedetekst(status)}
+                    {status}
                 </span>
             </div>
             }
