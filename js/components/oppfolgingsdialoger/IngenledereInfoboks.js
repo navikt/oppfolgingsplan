aaf58ac4385ae84e905bdcff4a1f44baecb320d7
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-26 10:37:30 +0100

    Replace Syfotekster with component texts in IngenledereInfoboks

--- js/components/oppfolgingsdialoger/IngenledereInfoboks.js
+++ js/components/oppfolgingsdialoger/IngenledereInfoboks.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { getLedetekst } from '@navikt/digisyfo-npm';
 import getContextRoot from '../../utils/getContextRoot';
 import OppfolgingsplanInfoboks from '../app/OppfolgingsplanInfoboks';
+
+const texts = {
+    title: 'Du kan ikke lage en oppfølgingsplan akkurat nå',
+    info: `
+        For å bruke denne tjenesten må vi vite hvem lederen din er slik at informasjonen kommer til rett person.
+        Ta kontakt med lederen din og be om at riktig leder blir registrert i Altinn.
+    `,
+};
 
 const IngenledereInfoboks = () => {
     return (<OppfolgingsplanInfoboks
         svgUrl={`${getContextRoot()}/img/svg/oppfolgingsdialog-ingenleder.svg`}
         svgAlt="Ingen Leder"
-        tittel={getLedetekst('oppfolgingsdialog.arbeidstaker.ingenledereInfoboks.tittel')}
-        tekst={getLedetekst('oppfolgingsdialog.arbeidstaker.ingenledereInfoboks.tekst')}
+        tittel={texts.title}
+        tekst={texts.info}
     />);
 };
 
