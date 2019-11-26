4b5166c0fdb4f2292f007d0624106aad148d1fc1
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-26 08:51:33 +0100

    Replace Syfotekster with component texts in OppfolgingsdialogPlanInfoboks

--- js/components/oppfolgingsplan/godkjenn/releasetplan/OppfolgingsdialogPlanInfoboks.js
+++ js/components/oppfolgingsplan/godkjenn/releasetplan/OppfolgingsdialogPlanInfoboks.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { getLedetekst } from '@navikt/digisyfo-npm';
+
+const texts = {
+    title: 'Hva skjer nå?',
+    info: {
+        paragraph1: `
+            Innen fire uker skal den som har sykmeldt deg få se planen.
+            Når du og arbeidsgiveren din har blitt enige om planen, kan dere dele den digitalt med både legen og NAV.
+        `,
+        paragraph2: 'NAV kan ikke se planen før dere har delt den. Dere kan dele den når dere selv ønsker. I tillegg kan NAV be arbeidsgiveren din om å få se den.',
+    },
+};
 
 const OppfolgingsdialogPlanInfoboks = () => {
-    const infoboksTittel = 'oppfolgingsdialog.arbeidstaker.hvaskjernaa.tittel';
-
-    const infoboksTekst = 'oppfolgingsdialog.arbeidstaker.hvaskjernaa.tekst';
-
     return (
         <div className="panel">
-            <h3 className="panel__tittel">{getLedetekst(infoboksTittel)}</h3>
-            <p dangerouslySetInnerHTML={{ __html: getLedetekst(infoboksTekst) }} />
+            <h3 className="panel__tittel">{texts.title}</h3>
+            <p>
+                {texts.info.paragraph1}
+                <br />
+                <br />
+                {texts.info.paragraph2}
+            </p>
         </div>);
 };
 
