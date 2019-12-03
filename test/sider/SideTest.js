0b0d6bc706c27978915669cf2a5f1ed7d4242dbc
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-12-03 09:04:33 +0100

    Fix ForlengInnloggetSesjon in TimeoutBox

--- test/sider/SideTest.js
+++ test/sider/SideTest.js
@@ -6,7 +6,7 @@ import sinon from 'sinon';
 import Feilmelding from '../../js/components/Feilmelding';
 import { SideComponent, Utlogget } from '../../js/sider/Side';
 import Brodsmuler from '../../js/components/Brodsmuler';
-import TimeoutBox from '../../js/components/app/timeout/TimeoutBox';
+import TimeoutBox from '../../js/timeout/TimeoutBox';
 
 const DocumentTitle = require('react-document-title');
 
