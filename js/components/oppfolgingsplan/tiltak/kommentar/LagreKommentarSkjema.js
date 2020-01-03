3fd173d2e2dd403ac3fa645e041f953329c3acce
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-01-03 14:04:58 +0100

    Remove redundant classname lenke--avbryt

--- js/components/oppfolgingsplan/tiltak/kommentar/LagreKommentarSkjema.js
+++ js/components/oppfolgingsplan/tiltak/kommentar/LagreKommentarSkjema.js
@@ -70,7 +70,7 @@ export const LagreKommentarKnapper = ({ avbryt, spinner }) => {
             </div>
             <div className="knapperad__element">
                 <button
-                    className="lenke lenke--avbryt"
+                    className="lenke"
                     type="button"
                     onKeyPress={(e) => { handleKeyPress(avbryt, e); }}
                     onMouseDown={avbryt}>
