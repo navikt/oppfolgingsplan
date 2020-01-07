0cc162104f1f571d8328892a56882c3fbfb888c2
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-01-07 10:31:26 +0100

    Suggest start and end of approved plan based on TiltakListe

--- test/utils/tiltakUtilsTest.js
+++ test/utils/tiltakUtilsTest.js
@@ -1,15 +1,32 @@
 import chai from 'chai';
+import sinon from 'sinon';
 import getTiltak from '../mock/mockTiltak';
 import {
     input2RSTiltak,
     sorterTiltakEtterNyeste,
     sorterKommentarerEtterOpprettet,
+    getEndDateFromTiltakListe,
+    getStartDateFromTiltakListe,
 } from '../../js/utils/tiltakUtils';
+import { leggTilDagerPaaDato } from '../testUtils';
+import { restdatoTildato } from '../../js/utils/datoUtils';
 
 const expect = chai.expect;
 
 describe('tiltakUtils', () => {
     const tiltak = getTiltak();
+
+    let clock;
+
+    beforeEach(() => {
+        const today = new Date('2000-01-15');
+        today.setHours(0);
+        clock = sinon.useFakeTimers(today.getTime());
+    });
+
+    afterEach(() => {
+        clock.restore();
+    });
 
     it('Skal kontrollre input2RSTiltak', () => {
         expect(input2RSTiltak(tiltak).tiltaknavn).to.deep.equal('Tiltak1');
@@ -54,6 +71,100 @@ describe('tiltakUtils', () => {
             ];
             const resultat = sorterKommentarerEtterOpprettet([...kommentarer]);
             expect(resultat[0].tekst).to.equal(kommentarer[kommentarer.length - 1].tekst);
+        });
+    });
+
+    describe('getStartDateFromTiltakListe', () => {
+        it('Skal hente tidligste fom fra liste over tiltak', () => {
+            const tiltakList = [
+                {
+                    fom: leggTilDagerPaaDato(new Date(), 5).toISOString(),
+                },
+                {
+                    fom: leggTilDagerPaaDato(new Date(), 1).toISOString(),
+                },
+                {
+                    fom: leggTilDagerPaaDato(new Date(), 10).toISOString(),
+                },
+            ];
+            const result = getStartDateFromTiltakListe([...tiltakList]);
+            const expected = restdatoTildato(tiltakList[1].fom);
+            expect(result).to.equal(expected);
+        });
+
+        it('Skal hente tidligste fom fra liste over tiltak, om det er tiltak med og uten fom dato', () => {
+            const tiltakList = [
+                {
+                    fom: null,
+                },
+                {
+                    fom: leggTilDagerPaaDato(new Date(), 1).toISOString(),
+                },
+                {
+                    fom: leggTilDagerPaaDato(new Date(), 10).toISOString(),
+                },
+            ];
+            const result = getStartDateFromTiltakListe([...tiltakList]);
+            const expected = restdatoTildato(tiltakList[1].fom);
+            expect(result).to.equal(expected);
+        });
+
+        it('Skal ikke returnere en dato om det ikke er noen tiltak med fom', () => {
+            const tiltakList = [
+                {
+                    fom: null,
+                },
+            ];
+            const result = getStartDateFromTiltakListe([...tiltakList]);
+            const expected = undefined;
+            expect(result).to.equal(expected);
+        });
+    });
+
+    describe('getEndDateFromTiltakListe', () => {
+        it('Skal hente siste tom fra liste over tiltak', () => {
+            const tiltakList = [
+                {
+                    tom: leggTilDagerPaaDato(new Date(), 5).toISOString(),
+                },
+                {
+                    tom: leggTilDagerPaaDato(new Date(), 10).toISOString(),
+                },
+                {
+                    tom: leggTilDagerPaaDato(new Date(), 1).toISOString(),
+                },
+            ];
+            const result = getEndDateFromTiltakListe([...tiltakList]);
+            const expected = restdatoTildato(tiltakList[1].tom);
+            expect(result).to.equal(expected);
+        });
+
+        it('Skal hente siste tom fra liste over tiltak, om det er tiltak med og uten tom dato', () => {
+            const tiltakList = [
+                {
+                    tom: null,
+                },
+                {
+                    tom: leggTilDagerPaaDato(new Date(), 10).toISOString(),
+                },
+                {
+                    tom: leggTilDagerPaaDato(new Date(), 1).toISOString(),
+                },
+            ];
+            const result = getEndDateFromTiltakListe([...tiltakList]);
+            const expected = restdatoTildato(tiltakList[1].tom);
+            expect(result).to.equal(expected);
+        });
+
+        it('Skal ikke returnere en dato om det ikke er noen tiltak med tom', () => {
+            const tiltakList = [
+                {
+                    tom: null,
+                },
+            ];
+            const result = getEndDateFromTiltakListe([...tiltakList]);
+            const expected = undefined;
+            expect(result).to.equal(expected);
         });
     });
 });
