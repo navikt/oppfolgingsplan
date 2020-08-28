1b0dd8a5ce39c4d36c900fd6fe23a48552100039
Sindre Broch <sindrebroch@Sindres-MBP.localdomain>
2020-08-28 12:40:49 +0200

    Fjerner for test

--- js/components/oppfolgingsplan/Oppfolgingsdialog.js
+++ js/components/oppfolgingsplan/Oppfolgingsdialog.js
@@ -120,7 +120,6 @@ class Oppfolgingsdialog extends Component {
         } else {
             (() => {
                 if (navigasjontoggles.steg === 1) {
-                    skalViseAvsluttOgLagre = true;
                     panel = (<Arbeidsoppgaver
                         arbeidsoppgaver={arbeidsoppgaver}
                         oppfolgingsdialog={oppfolgingsdialog}
@@ -128,7 +127,6 @@ class Oppfolgingsdialog extends Component {
                         slettArbeidsoppgave={slettArbeidsoppgave}
                     />);
                 } else if (navigasjontoggles.steg === 2) {
-                    skalViseAvsluttOgLagre = true;
                     panel = (<Tiltak
                         tiltak={tiltak}
                         oppfolgingsdialog={oppfolgingsdialog}
