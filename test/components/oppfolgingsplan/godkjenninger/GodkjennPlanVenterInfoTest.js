f77d49baba43b1f053c1ce804ef992512b849d77
Peter Stensby <peter.stensby@nav.no>
2020-09-28 14:08:45 +0200

    Rydd opp i imports

--- test/components/oppfolgingsplan/godkjenninger/GodkjennPlanVenterInfoTest.js
+++ test/components/oppfolgingsplan/godkjenninger/GodkjennPlanVenterInfoTest.js
@@ -2,15 +2,7 @@ import React from 'react';
 import chai from 'chai';
 import { shallow } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
-import { Utvidbar } from '@navikt/digisyfo-npm';
 import AlertStripe from 'nav-frontend-alertstriper';
-import GodkjennPlanSendt, {
-    GodkjennPlanSendtInfoTekst,
-    GodkjennPlanSendtUtvidbar,
-} from '../../../../js/components/oppfolgingsplan/godkjenn/godkjenninger/GodkjennPlanSendt';
-import OppfolgingsplanInnholdboks from '../../../../js/components/app/OppfolgingsplanInnholdboks';
-import GodkjennPlanOversiktInformasjon from '../../../../js/components/oppfolgingsplan/godkjenn/godkjenn/GodkjennPlanOversiktInformasjon';
-import getOppfolgingsdialog from '../../../mock/mockOppfolgingsdialog';
 import GodkjennPlanVenterInfo from '../../../../js/components/oppfolgingsplan/godkjenn/godkjenn/GodkjennPlanVenterInfo';
 
 chai.use(chaiEnzyme());
