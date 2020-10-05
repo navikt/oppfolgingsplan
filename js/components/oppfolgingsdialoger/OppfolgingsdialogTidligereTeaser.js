8c26ae1ccf4b0572b9b45adf9cc046c0204f55b8
Sindre Broch <sindrebroch@Sindres-MBP.localdomain>
2020-10-05 11:30:39 +0200

    Reverserer mellomrom mellom lenkepanel

--- js/components/oppfolgingsdialoger/OppfolgingsdialogTidligereTeaser.js
+++ js/components/oppfolgingsdialoger/OppfolgingsdialogTidligereTeaser.js
@@ -15,7 +15,6 @@ const OppfolgingsdialogTidligereTeaser = (
     return (
         <LenkepanelBase
             href={`${rootUrlPlaner}/oppfolgingsplaner/${oppfolgingsdialog.id}`}
-            className="inngangspanel--lenkepanel"
             border>
             <div className="inngangspanel">
                 <span className="oppfolgingsplanInnhold__ikon">
