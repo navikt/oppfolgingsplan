a0891c25e344bccc9daaca195c275bf30283b84f
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-01-20 09:56:57 +0100

    Universell utforming: Endret fra "valgfri" til "valgfritt" i infotekst

--- js/components/oppfolgingsplan/ObligatoriskeFelterInfotekst.js
+++ js/components/oppfolgingsplan/ObligatoriskeFelterInfotekst.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const obligatoriskeFelter = 'I utgangspunktet må alle feltene være utfylt for å kunne gå videre. Når noen felt er frivillige å fylle ut, har vi markert dem med "valgfri".';
+const obligatoriskeFelter = 'I utgangspunktet må alle feltene være utfylt for å kunne gå videre. Når noen felt er frivillige å fylle ut, har vi markert dem med "valgfritt".';
 
 const ObligatoriskeFelterInfotekst = () => {
     return (
