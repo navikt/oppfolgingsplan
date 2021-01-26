01eaa1e8b9f14ce9a538821c8919528e833b438f
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-01-26 12:34:39 +0100

    Lagt til border når man legger til et nytt tiltak

--- js/components/oppfolgingsplan/tiltak/TiltakSkjema.js
+++ js/components/oppfolgingsplan/tiltak/TiltakSkjema.js
@@ -197,6 +197,10 @@ export class TiltakSkjemaKomponent extends Component {
         this.props.avbryt();
     }
 
+    border() {
+        return !this.props.tiltak;
+    }
+
     render() {
         const {
             tiltak,
@@ -208,6 +212,7 @@ export class TiltakSkjemaKomponent extends Component {
             tiltakReducer,
         } = this.props;
         return (
+            <Panel border={this.border()}>
                 <div className="utvidbar__innholdContainer">
                     <form onSubmit={handleSubmit(this.handleSubmit)} className={this.hentSkjemaClassName()} >
 
@@ -246,7 +251,8 @@ export class TiltakSkjemaKomponent extends Component {
                             tiltakReducer={tiltakReducer}
                         />
                     </form>
-                </div>  );
+                </div>
+            </Panel>);
     }
 }
 
