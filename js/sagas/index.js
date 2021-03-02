2bd0a76d0f3921bbee500a2b271b9922ef6639cd
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-03-02 13:07:39 +0100

    37gppQFB: Fjernet alt relatert til syforest sin sykeforloep-tjeneste da det ser ut som den ikke er brukt

--- js/sagas/index.js
+++ js/sagas/index.js
@@ -19,7 +19,6 @@ import tiltakSagas from './oppfolgingsplan/tiltakSagas';
 import virksomhetSagas from './oppfolgingsplan/virksomhetSagas';
 
 import dineSykmeldingerSagas from './dineSykmeldingerSagas';
-import sykeforloepSagas from './sykeforloepSagas';
 import ledereSagas from './ledereSagas';
 
 export default function* rootSaga() {
@@ -43,7 +42,6 @@ export default function* rootSaga() {
         personSagas(),
         kontaktinfoSagas(),
         naermesteLederSagas(),
-        sykeforloepSagas(),
         ledereSagas(),
     ]);
 }
