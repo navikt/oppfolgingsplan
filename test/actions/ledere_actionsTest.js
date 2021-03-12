4384a406ba2b0f5d59807f9756f46583386a1051
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-03-12 14:04:24 +0100

    r8OkRxEI: fikset tester som feiler

--- test/actions/ledere_actionsTest.js
+++ test/actions/ledere_actionsTest.js
@@ -5,9 +5,10 @@ import * as actiontyper from '../../js/actions/actiontyper';
 
 describe('ledere_actions', () => {
     it('Har en hentLedere()-funksjon', () => {
-        const res = actions.hentLedere();
+        const res = actions.hentLedere('12121212121');
         expect(res).to.deep.equal({
             type: actiontyper.HENT_LEDERE_FORESPURT,
+            fodselsnummer: '12121212121'
         });
     });
 });
