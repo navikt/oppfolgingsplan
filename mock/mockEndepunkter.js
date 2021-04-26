9b8a5431943da808faf5b3cf4796a645816132aa
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-26 21:43:45 +0200

    Jdfxfptn: Slettet ubrukt variabel i mock

--- mock/mockEndepunkter.js
+++ mock/mockEndepunkter.js
@@ -1,18 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-const request = require('request');
 const express = require('express');
 
 const mockOppfolgingsplan = require('./oppfolgingsplan/mockOppfolgingsplan');
 const dateUtil = require('./util/dateUtil');
-
-const uuid = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
 
 const mockData = {};
 const ARBEIDSGIVERE = 'arbeidsgivere';
