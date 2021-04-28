a21160a8df9f6125efa0549071c73ea4f685a9d5
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-04-28 22:31:15 +0200

    XMhZBz0v:Utvidet ny regex

--- js/konstanter.js
+++ js/konstanter.js
@@ -1,5 +1,5 @@
 export const tekstfeltRegex = new RegExp('.*<[^ ][^>]+[^ ]>.*');
-export const tekstfeltBegynnerMedUgyldigTegnRegex = new RegExp('^.*[^a-zA-Z0-9]');
+export const tekstfeltBegynnerMedUgyldigTegnRegex = new RegExp('^[^a-zA-Z0-9]+[0-9A-Za-z:;., !$?\\-_]');
 export const maanedListe = [
   'januar',
   'februar',
