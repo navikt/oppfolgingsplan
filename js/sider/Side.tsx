3af029037e122edb6b75fc9c86e034140a2df12f
Tien Quoc Tran <tien.tran@nav.no>
2022-08-16 10:13:00 +0200

    feat: display general error alert

--- js/sider/Side.tsx
+++ js/sider/Side.tsx
@@ -1,6 +1,7 @@
 import { Row } from 'nav-frontend-grid';
 import React, { ReactElement } from 'react';
 import DocumentTitle from 'react-document-title';
+import { AlertStripeFeil } from "nav-frontend-alertstriper";
 import Brodsmuler from '@/components/Brodsmuler';
 
 interface SideProps {
@@ -9,11 +10,18 @@ interface SideProps {
   children: ReactElement;
 }
 
+const texts = {
+    errorMsg: "Det er en teknisk feil med oppfølgingsplanen. Vi jobber med å løse problemet. Prøv igjen senere.",
+}
+
 const Side = ({ tittel, brodsmuler, children }: SideProps): ReactElement => {
   return (
     <DocumentTitle title={tittel + (tittel.length > 0 ? ' - www.nav.no' : 'www.nav.no')}>
       <div className={'side__innhold side__innhold--begrenset'}>
         <Brodsmuler brodsmuler={brodsmuler} />
+          <AlertStripeFeil className="margin-bottom" >
+              {texts.errorMsg}
+          </AlertStripeFeil>
         <Row>{children}</Row>
       </div>
     </DocumentTitle>
